da8e2d45dff416807ae0db630d562a45a1c31599
Pierre Romera <hello@pirhoo.com>
2012-11-17 17:38:17 +0100

    Updated allowed sites for timthumb

--- pirhoo-core/library/timthumb-config.php
+++ pirhoo-core/library/timthumb-config.php
@@ -6,6 +6,6 @@
 	define ('FILE_CACHE_ENABLED', TRUE);
 	define ('FILE_CACHE_DIRECTORY', is_cctrl() ? $_SERVER["TMPDIR"] : "../cache/");
 
-	$ALLOWED_SITES = array("branchetonsonotone.com", "oeildupirate.com", "pirhoo.dev", "127.0.0.1");
+	$ALLOWED_SITES = array("branchetonsonotone.com", "oeildupirate.com", "pirhoo.dev", "127.0.0.1", "jquest.s3.amazonaws.com");
 
 ?>
