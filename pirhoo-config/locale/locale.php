b94c11642f503220a2263428d6f365b34abfeeed
pirhoo <pirhoo@sebulba.(none)>
2012-12-09 23:18:44 +0100

    Addes translations

---
+++ pirhoo-config/locale/locale.php
@@ -0,0 +1,44 @@
+
+<?php
+gettext("Contact");
+gettext("Qui suis-je ?");
+gettext("Mes travaux");
+gettext("Tous");
+gettext("Data-Journalism / Webapp");
+gettext("Site");
+gettext("Cours / Conf&eacute;rence");
+gettext("Mes articles");
+
+?>
+
+<?php
+
+?>
+
+<?php
+gettext("Pierre Romera");
+gettext("Qui suis-je ?");
+gettext("Directeur G&eacute;n&eacute;ral et co-fondateur de <a href=\"http://jplusplus.org\" class=\"open-window\">Journalism++</a>");
+gettext("Journalism++ accompagne les r&eacute;dactions dans leur transition vers le web des donn&eacute;es.");
+gettext("Enseignant &agrave; l'<a href=\"http://www.journalisme.sciences-po.fr\" target=\"_blank\">&Eacute;cole de Journalisme de Science Po</a>");
+gettext("J'initie les journalistes au langage et au d&eacute;veloppement num&eacute;rique.");
+gettext("Intervenant &agrave; l'<a href=\"http://esj-lille.fr/\" class=\"open-window\">ESJ Lille</a> et au <a href=\"http://www.cfpj.com/ecole-de-journalisme/\" class=\"open-window\">CFJ Paris</a>");
+gettext("J'interviens en &eacute;cole, en entreprise et en conf&eacute;rence pour parler des sujets li&eacute;s au <a href=\"https://fr.wikipedia.org/wiki/Journalisme_de_donn%C3%A9es\" class=\"open-window\">Journalisme de donn&eacute;es</a>.");
+gettext("D&eacute;veloppeur Freelance");
+gettext("Node.js, Express, Underscore, API, WordPress, jQuery, PHP, HTML5, Data Visualisation, ...");
+gettext("Directeur G&eacute;n&eacute;ral D&eacute;l&eacute;gu&eacute; chez <a href=\"http://owni.fr\" class=\"open-window\">OWNI</a>/<a href=\"http://22mars.com\" class=\"open-window\">22mars</a>");
+gettext("OWNI est un media en ligne, pionnier fran&ccedil;ais du Data Journalism.");
+gettext("Pierre Romera");
+gettext("Mes travaux");
+gettext("Pierre Romera");
+gettext("Mes articles");
+
+?>
+
+<?php
+
+?>
+
+<?php
+
+?>
